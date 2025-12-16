1e6c6e488c9e08dedcb9e63215b2fe0c1cac14f2
aslam <aslampatwary@gmail.com>
2025-12-17 03:56:06 +0600

    UI Completed

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,11 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  TextEditingController _titleController = TextEditingController();
+  TextEditingController _msgController = TextEditingController();
+
+  List<MsgObj> _posts = [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,14 +40,64 @@ class _MainPageState extends State<MainPage> {
               Expanded(
                 child: Container(
                   padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-                  child: Column(children: [TextField(), TextField()]),
+                  child: Column(
+                    children: [
+                      TextField(
+                        controller: _titleController,
+                        decoration: InputDecoration(hint: Text('Title')),
+                      ),
+                      TextField(
+                        controller: _msgController,
+                        decoration: InputDecoration(hint: Text('Message')),
+                      ),
+                    ],
+                  ),
                 ),
               ),
-              ElevatedButton(onPressed: () {}, child: Text('Add')),
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    _posts.add(
+                      MsgObj(
+                        title: _titleController.text,
+                        msg: _msgController.text,
+                      ),
+                    );
+                    [_titleController, _msgController].forEach((element) {
+                      element.clear();
+                    });
+                  });
+                },
+                child: Text('Add'),
+              ),
             ],
+          ),
+          Expanded(
+            child: ListView.builder(
+              reverse: true,
+              itemCount: _posts.length,
+              itemBuilder: (context, index) => ListTile(
+                leading: Text(index.toString()),
+                title: Text(_posts[index].title),
+                subtitle: Text(_posts[index].msg),
+                trailing: Row(
+                  children: [
+                    IconButton(onPressed: () {}, icon: Icon(Icons.edit)),
+                    IconButton(onPressed: () {}, icon: Icon(Icons.delete)),
+                  ],
+                ),
+              ),
+            ),
           ),
         ],
       ),
     );
   }
+}
+
+class MsgObj {
+  final String title;
+  final String msg;
+
+  MsgObj({required this.title, required this.msg});
 }
